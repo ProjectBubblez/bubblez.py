c2b94505d0ee805f5d6e32f756905d31b97f9835
MeesMeijer <72513766+MeesMeijer@users.noreply.github.com>
2021-09-29 00:26:45 +0200

    Add files via upload

---
+++ Bubblez/models/user.py
@@ -0,0 +1,62 @@
+import requests
+from requests.api import head
+from .post import Post
+from .reply import Reply
+
+stand_header = {"Content-Type": "application/x-www-form-urlencoded"}
+
+class User:
+    last_request = None 
+    stand_header = {"Content-Type": "application/x-www-form-urlencoded"}
+
+    def __init__(self, username, token) -> None:
+        self.username = username
+        self.token = token
+    
+    def json(self):
+        return {
+            "username": self.username,
+            "token": self.token,
+            "last_request": self.last_request
+        }
+    
+    def check(self):
+        resp = requests.post(
+            "https://bubblez.app/api/v1/user/check",
+            data={
+                "token": self.token
+            },
+            headers=self.stand_header
+        )
+        if resp.ok:
+            try:
+                return resp.json()
+            except:
+                print("Iets ging fout met user/check.. Status_code:", resp.status_code)
+                print("Content: ", resp.content)
+                return False 
+                
+        return False
+
+    def ping(self):
+        if not self.token:
+            raise ValueError("Missing token!")
+        print("Ping!")
+        resp = requests.post(
+            "https://bubblez.app/api/v1/user/ping", 
+            data={
+                "token": self.token
+            }, 
+            headers=self.stand_header
+        )
+        if resp.ok:
+            print("Pong!")
+       
+            self.username = resp.json()['username']
+            print(f"username: {self.username}")
+            print("everything is good! , status code: 200")
+            return resp
+            
+        else:
+            print("Something when\'t wrong! | status code:", resp.status_code, "byte_like content:", resp.content)
+            return resp 
