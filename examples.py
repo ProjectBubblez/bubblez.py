9dab8f94a50eb4f10635de4fc10bc615be5d8635
MeesMeijer <72513766+MeesMeijer@users.noreply.github.com>
2021-09-29 18:49:46 +0200

    Update examples.py

--- examples.py
+++ examples.py
@@ -4,7 +4,7 @@ from bubblez import Bubblez
 
 client = Bubblez("foo","Your Api token")
 
-# All funcs for posts
+
 def posts():
     # make a post: 
     client.post.send(
@@ -15,18 +15,18 @@ def posts():
     )
 
     # delete a post:
-    client.post.delete("The best postid got!")
+    client.post.delete(postid=...)
 
     # get a post:
-    client.post.get("Again with a postid..")
+    client.post.get(postid=...)
 
     # get the latest post or only the id of the post
     client.post.latest(postid_only=True or False)
 
     # and lock it with.. 
-    client.post.lock("postid..", locked=True or False)
+    client.post.lock(postid=..., locked=True or False)
 
-# All funcs for User
+
 def user():
 
     # ping / check the users token:
@@ -35,12 +35,11 @@ def user():
 
     # more is in the making but still cooking.
 
-# All funcs for reply's 
 def replys():
 
     # send a reply:
     client.reply.send(
-        postid="postid..", 
+        postid=..., 
         message="The beauti reply on this beauti message..", 
         from_="from Foo", 
         nsfw=True or False
@@ -48,11 +47,9 @@ def replys():
 
     # delete a reply:
     client.reply.delete(
-        replyid="U need some id?",
-        confirm=True
+        replyid=...
     )
 
-# Future plans:
 def Future_Plans():
 
     '''
@@ -64,9 +61,4 @@ def Future_Plans():
     last prio: making a UI
 
     '''
-
-
-
-
-
 
